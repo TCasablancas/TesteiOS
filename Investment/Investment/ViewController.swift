efbe271053ba255f86b1b8f74189404dd08d54e5
Thiago Silva <thyago_casablancas@hotmail.com>
2019-09-13 13:07:45 -0300

    Intializing project
    Creating project at XCode and starting over.

---
+++ Investment/Investment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Investment
+//
+//  Created by Thyago on 13/09/19.
+//  Copyright © 2019 tcasablancas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
